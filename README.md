86dd430992d6811f4e5b5baebdecd26f8ce9e3c3
yuliefas <yuliefas@gmail.com>
2020-10-08 11:55:12 +0700

    webpack simple skeleton for chrome extension

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # ngecilin
+
 [![Made in Indonesia](https://made-in-indonesia.github.io/made-in-indonesia.svg)](https://github.com/made-in-indonesia/made-in-indonesia)
 
 Chrome Extension to shorten your looooong URL
+
+## documentation
+
+- create `manifest.key` see [manifest docs](https://developer.chrome.com/extensions/manifest/key) used to development mode
+
+- first copy `manifest.key.example` to `key/manifest.key`
+
+```txt
+cp key/manifest.key.example key/manifest.key
+
+```
+
+- get your key and paste to `key/manifest.key`
---
+++ src/manifest.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+
+const manifest = {
+  name: 'Ngecilin Extension',
+  version: process.env.VERSION,
+  description: 'Chrome Extension to shorten your looooong URL.',
+  manifest_version: 2,
+  permissions: [],
+  content_security_policy: "script-src 'self'; object-src 'self'",
+  options_page: 'options/index.html',
+  background: {
+    scripts: ['background/index.js'],
+    persistent: true,
+  },
+  web_accessible_resources: [],
+  content_scripts: [],
+  browser_action: {
+    default_title: 'Ngecilin Extension',
+    default_icon: {
+      16: 'assets/icons/icn-16.png',
+      32: 'assets/icons/icn-32.png',
+      48: 'assets/icons/icn-48.png',
+      128: 'assets/icons/icn-128.png',
+    },
+  },
+  commands: {
+    _execute_browser_action: {
+      suggested_key: {
+        default: 'Ctrl+Shift+P',
+        mac: 'MacCtrl+Shift+P',
+      },
+      description: 'Open Ngecilin Extension',
+    },
+  },
+  icons: {
+    16: 'assets/icons/icn-16.png',
+    32: 'assets/icons/icn-32.png',
+    48: 'assets/icons/icn-48.png',
+    128: 'assets/icons/icn-128.png',
+  },
+  // oauth2: {
+  //   client_id: process.env.OAUTH2_CLIENT_ID,
+  //   scopes: ['profile',],
+  // },
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const fs = require('fs');
+  const path = require('path');
+  const key = path.resolve('key', 'manifest.key');
+  manifest.key = fs.readFileSync(key, 'utf8');
+}
+
+module.exports = {
+  manifest,
+  filters: Object.keys(manifest)
+};
